b6de166962f26118806e87e27f2ad9cc03946018
Jari Anttila <jari.anttila@innofactor.com>
2023-07-26 14:45:07 +0300

    Add project files.

---
+++ Pages/Ilmo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HaatWeb.Pages
+{
+    public class IlmoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
